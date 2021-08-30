21234ee323a43d18c33602262989688224084f64
Mariell Hoversholm <proximyst@proximyst.com>
2021-08-30 08:05:40 +0200

    chore: generate project
    This commit is unlicensed and freely relicensable to any licence approved by
    the Open Source Initiative. This right is only granted to contributors
    and members of Discord Portier.
    
    Signed-off-by: Mariell Hoversholm <proximyst@proximyst.com>

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.github.discordportier"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
