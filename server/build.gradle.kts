9d79200a172dcd1eddb9efcf8dcda12db53c5869
Mariell Hoversholm <proximyst@proximyst.com>
2021-08-30 09:04:17 +0200

    feat: add runnable server main
    This commit is unlicensed and freely relicensable to any licence
    approved by the Open Source Initiative. This right is only
    granted to contributors and members of Discord Portier.
    
    Signed-off-by: Mariell Hoversholm <proximyst@proximyst.com>

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,3 +1,14 @@
 plugins {
     id("portier.impl-conventions")
+    application
+}
+
+dependencies {
+    implementation(rootProject.libs.bundles.log4j.impl)
+    implementation(rootProject.libs.kotlin.logging)
+    implementation(rootProject.libs.bundles.jackson)
+}
+
+application {
+    mainClass.set("com.discordportier.server.MainKt")
 }
---
+++ server/src/main/kotlin/com/discordportier/server/main.kt
@@ -0,0 +1,11 @@
+@file:JvmName("MainKt")
+
+package com.discordportier.server
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger { }
+
+fun main() {
+    logger.info("Application starting up...")
+}
