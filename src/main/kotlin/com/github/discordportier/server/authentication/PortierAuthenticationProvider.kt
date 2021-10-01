b1026273b60386c14c80254a29ec7bcd495e6e67
Mariell Hoversholm <proximyst@proximyst.com>
2021-10-01 23:59:34 +0200

    refactor: rename {logger => klogger} for consistency
    Signed-off-by: Mariell Hoversholm <proximyst@proximyst.com>

--- src/main/kotlin/com/github/discordportier/server/authentication/PortierAuthenticationProvider.kt
+++ src/main/kotlin/com/github/discordportier/server/authentication/PortierAuthenticationProvider.kt
@@ -2,14 +2,14 @@ package com.github.discordportier.server.authentication
 
 import com.github.discordportier.server.model.auth.AuthenticatedUser
 import com.github.discordportier.server.service.UserAuthenticationService
+import java.util.UUID
 import mu.KotlinLogging
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
-import java.util.*
 
-private val logger = KotlinLogging.logger { }
+private val klogger = KotlinLogging.logger { }
 
 @Component
 class PortierAuthenticationProvider(
@@ -17,7 +17,7 @@ class PortierAuthenticationProvider(
 ) : AuthenticationProvider {
     override fun authenticate(authentication: Authentication): Authentication {
         if (authentication !is AuthenticatedUser) {
-            logger.debug { "Authentication is not AuthenticatedUser: $authentication" }
+            klogger.debug { "Authentication is not AuthenticatedUser: $authentication" }
             throw BadCredentialsException("Not AuthenticatedUser")
         }
 
@@ -27,12 +27,12 @@ class PortierAuthenticationProvider(
             throw BadCredentialsException("Invalid user id", ex)
         }
         val password = authentication.credentials.credentials.toString()
-        logger.debug { "Authenticating $userId:$password" }
+        klogger.debug { "Authenticating $userId:$password" }
 
         val user = userAuthenticationService.authenticate(userId, password)
             ?: throw BadCredentialsException("Could not authenticate for user: $userId")
 
-        logger.debug { "Authentication successful for $userId" }
+        klogger.debug { "Authentication successful for $userId" }
         return authentication.copy(user = user)
     }
 
