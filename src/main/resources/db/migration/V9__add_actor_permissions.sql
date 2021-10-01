a1bd05334a0757f92a76f7f80053c6b1327f04fe
Mariell Hoversholm <proximyst@proximyst.com>
2021-10-02 00:14:32 +0200

    fix: alter types must be separate
    Signed-off-by: Mariell Hoversholm <proximyst@proximyst.com>

--- src/main/resources/db/migration/V9__add_actor_permissions.sql
+++ src/main/resources/db/migration/V9__add_actor_permissions.sql
@@ -1,4 +1,4 @@
 ALTER TYPE portier.user_permission
-    ADD VALUE 'READ_ACTORS',
-    ADD VALUE 'CREATE_ACTORS'
-;
+    ADD VALUE 'READ_ACTORS';
+ALTER TYPE portier.user_permission
+    ADD VALUE 'CREATE_ACTORS';
